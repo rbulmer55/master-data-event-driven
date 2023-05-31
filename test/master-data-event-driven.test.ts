0bf0c4dc8e657c7451a1eaa883cad3e5c963c010
Rob Bulmer <Robert.bulmer@cef.co.uk>
2023-05-31 20:58:04 +0100

    Initial commit

---
+++ test/master-data-event-driven.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as MasterDataEventDriven from '../lib/master-data-event-driven-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/master-data-event-driven-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new MasterDataEventDriven.MasterDataEventDrivenStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
